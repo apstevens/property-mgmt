69a12feac3eb59a94987250f32999768aee09f1a
apstevens <andypaulstevens@gmail.com>
2023-06-28 21:01:45 +0100

    Implemented error handling using AOP

--- property-management/src/main/java/com/shayledesignsolutions/propertymanagement/controller/UserController.java
+++ property-management/src/main/java/com/shayledesignsolutions/propertymanagement/controller/UserController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
@@ -17,7 +19,7 @@ public class UserController {
   @Autowired
   private UserService userService;
   @PostMapping("/register")
-  public ResponseEntity<UserDTO> saveUser(@RequestBody UserDTO userDTO) {
+  public ResponseEntity<UserDTO> saveUser(@Valid @RequestBody UserDTO userDTO) {
     userDTO = userService.register(userDTO);
     return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
   }
--- property-management/src/main/java/com/shayledesignsolutions/propertymanagement/dto/UserDTO.java
+++ property-management/src/main/java/com/shayledesignsolutions/propertymanagement/dto/UserDTO.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import javax.persistence.Column;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,7 +17,12 @@ import lombok.Setter;
 public class UserDTO {
   private Long id;
   private String ownerName;
+  @NotNull(message = "Owner email is mandatory")
+  @NotEmpty(message = "Owner email cannot be empty")
+  @Size(min = 1, max = 50, message = "Owner email should be between 1 to 50 characters in length")
   private String ownerEmail;
   private String phone;
+  @NotNull(message = "Password cannot be null")
+  @NotEmpty(message = "Password cannot be empty")
   private String password;
 }
